41054165d432bc0c047c0382194daa71efe7408c
Ihor Ivliev <igor852745@gmail.com>
2020-03-25 13:20:07 +0200

    AddTodo commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import TodoList from "./Todo/TodoList";
+import Context from "./context";
+import {object} from "prop-types";
+import AddTodo from "./Todo/AddTodo";
 
 function App() {
 
     const [todos,setTodos] = React.useState(
         [
             {id:1, completed:false, title: 'Купить хлеб'},
-            {id:2, completed:true, title: 'Купить масло'},
+            {id:2, completed:false, title: 'Купить масло'},
             {id:3, completed:false, title: 'Купить молоко'},
 
         ]
@@ -24,12 +27,33 @@ function App() {
         )
     }
 
+
+    function removeTodo(id) {
+
+        setTodos(todos.filter(todo => todo.id !== id))
+
+    }
+
+    
+    function addTodo(title) {
+
+            setTodos(todos.concat([{
+                title,
+                id:Date.now(),
+                completed:false
+            }]))
+    }
+
+    
   return (
+      <Context.Provider value={{removeTodo}}>
       <div className='wrapper'>
             <h1>react tutorial</h1>
+            <AddTodo onCreate={addTodo}/>
+          {todos.length ? (<TodoList todos={todos} onToggle={toggleTodo}/>) : (<p>No Todo!</p>) }
 
-          <TodoList todos={todos} onToggle={toggleTodo}/>
       </div>
+      </Context.Provider>
   )
 }
 
